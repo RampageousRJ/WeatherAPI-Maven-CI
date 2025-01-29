31d74d2072e0b4a262e57b8ff10fb377209bfcfb
Rishabh Jain <rishabh.z.jain@oracle.com>
2025-01-29 14:56:14 +0530

    Unit Test

--- src/test/java/weatherapi/AppTest.java
+++ src/test/java/weatherapi/AppTest.java
@@ -1,20 +1,34 @@
 package weatherapi;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import java.net.URI;
+import java.net.URL;
+import java.net.HttpURLConnection;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class AppTest {
+
+    @Mock
+    private HttpURLConnection connection;
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void isAPIKeyValid() throws Exception {
+        Dotenv dotenv = Dotenv.load();
+        final String API_KEY = dotenv.get("API_KEY");
+        final String API_URL = dotenv.get("API_ENDPOINT");
+        final String city = "London";
+        
+        URI uri = new URI(API_URL + "?key=" + API_KEY + "&q=" + city);
+        URL url = uri.toURL();
+        assertDoesNotThrow(() -> {
+            connection = (HttpURLConnection) url.openConnection();
+        });
+        assertNotNull(url);
+        assertTrue(url.toString().contains(API_KEY)); // Ensure the API key is present in the URL
     }
 }
+
+
